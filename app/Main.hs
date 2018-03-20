df9f3533e0234b266c5c9646b30d04ad9b74462d
Mikhail <mitutee@gmail.com>
2018-03-20 08:34:02 +0000

    Fix and add logging

--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,26 @@
 module Main where
 
 import Control.Concurrent (threadDelay)
+import Data.DateTime (getCurrentTime)
 
 import Uhouse.Hardware.DS18B20
 
-myLog :: String -> IO ()
-myLog = putStrLn
+myLog :: Maybe Temperature -> IO ()
+myLog t = do
+    currTime <- getCurrentTime
+    let logM = show (currTime, t) ++ "\n"
+    appendFile "temperature.log" logM
+    putStr logM
 
 main :: IO ()
 main = do
-    initModprobe
+    -- initModprobe
     file <- getTemperatureFile
+    putStrLn $ show file
     go file
       where
         go tempFile = do
             temp <- readTemp tempFile
-            myLog $ show temp
+            myLog temp
             threadDelay 1000000
             go tempFile
--- src/Uhouse/Hardware/DS18B20.hs
+++ src/Uhouse/Hardware/DS18B20.hs
@@ -10,6 +10,7 @@ where
 import Data.Foldable (asum)
 import Data.List (find, isPrefixOf, stripPrefix)
 import System.Directory (getDirectoryContents)
+import System.FilePath ((</>))
 import System.Process (callCommand)
 
 initModprobe :: IO ()
@@ -17,15 +18,18 @@ initModprobe = do
     callCommand "modprobe w1-gpio"
     callCommand "modprobe w1-therm"
 
+rootDeviceDir :: FilePath
+rootDeviceDir = "/sys/bus/w1/devices/"
+
 getTemperatureFile :: IO FilePath
-getTemperatureFile = pickTempFile <$> getDirectoryContents "/sys/bus/w1/devices/"
+getTemperatureFile = pickTempFile <$> getDirectoryContents rootDeviceDir
   where
     pickTempFile :: [FilePath] -> FilePath
     pickTempFile ps = case find (isPrefixOf "10") ps of
-        Just dn -> dn
+        Just dn -> rootDeviceDir </> dn </> "w1_slave"
         Nothing -> error "No device found"
 
-newtype Temperature = RawTemperature { getRawTemp :: String } deriving (Show)
+newtype Temperature = RawTemperature { getRawTemp :: String } deriving (Show, Read)
 
 readTemp :: FilePath -> IO (Maybe Temperature)
 readTemp fp = mapTemp <$> readFile fp
