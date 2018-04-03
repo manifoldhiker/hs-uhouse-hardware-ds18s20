79df58a961a0517f2e2997689754cedb5829e98e
Mikhail <mitutee@gmail.com>
2018-04-03 08:30:54 +0000

    Fix build

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,13 @@
 import Data.DateTime (getCurrentTime)
 
+import Control.Concurrent (threadDelay)
 import Uhouse.Hardware.DS18B20
 
 myLog :: Maybe Temperature -> IO ()
 myLog t = do
     currTime <- getCurrentTime
     let logM = show (currTime, t) ++ "\n"
-    appendFile "/home/pi/temperature.ds18s20.log" logM
+    -- appendFile "/home/pi/temperature.ds18s20.log" logM
     putStr logM
 
 main :: IO ()
